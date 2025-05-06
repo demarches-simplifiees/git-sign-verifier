598e24000e618c75f41771bfe737e0f62e4b680f
Colin Darie <colin@darie.eu>
2025-05-06 12:36:59 +0200

    chore(deps): +clap and basic init command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Initialise le dépôt en crééant le tag de référence sur le dernier commit de la branche `main`.
+    Init {},
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
 }
